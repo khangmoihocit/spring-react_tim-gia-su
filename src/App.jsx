3445b0405b387e22b52e2e805d9cb74a0b859468
khangmoihocit <23a1001d0196@students.hou.edu.vn>
2025-05-16 20:35:15 +0700

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState } from 'react'
+import CourseList from './components/course/CourseList'
 
 
 
 function App() {
-  
+  return (
+    <>
+      <CourseList />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/course/CourseList.jsx
@@ -0,0 +1,7 @@
+export default function CourseList(){
+    return (
+        <>
+            <div>danh sach khoa hoc</div>
+        </>
+    )
+}
